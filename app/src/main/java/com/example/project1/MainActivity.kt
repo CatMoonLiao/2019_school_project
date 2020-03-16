7279226695dc1a1aaa29fd3eeb4106d6f0d1d112
CatMoonLiao <nicole951527@gmail.com>
2020-03-16 22:26:29 +0800

    月計畫版面done

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.month_activity_home)
+        val calender = Calendar.getInstance(TimeZone.getDefault(),Locale.TAIWAN)
     }
 }
