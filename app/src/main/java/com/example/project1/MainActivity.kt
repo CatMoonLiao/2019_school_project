1f565ea0a23377ff60dfc3518be8963de3ffa936
CatMoonLiao <nicole951527@gmail.com>
2020-03-17 23:07:58 +0800

    fragment done ,bar disappeared(setting?)

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -6,53 +6,74 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val manager = supportFragmentManager
-    val transaction = supportFragmentManager.beginTransaction()
 
-    public var listener = object : BottomNavigationView.OnNavigationItemSelectedListener {
+    private var listener = object : BottomNavigationView.OnNavigationItemSelectedListener {
         override fun onNavigationItemSelected(item: MenuItem): Boolean {
             when (item.itemId) {
                 R.id.menu_month -> {
-                    Log.e("month","!!!")
+                    val transaction = manager.beginTransaction()
+
                     transaction.replace(R.id.frameLayout, MonthFragment()).commit()
                     return true
                 }
                 R.id.menu_week -> {
+                    val transaction = manager.beginTransaction()
                     transaction.replace(R.id.frameLayout, WeekFragment()).commit()
                     return true
                 }
                 R.id.menu_day -> {
+                    val transaction = manager.beginTransaction()
                     transaction.replace(R.id.frameLayout, DayFragment()).commit()
                     return true
                 }
                 R.id.menu_new -> {
+                    val transaction = manager.beginTransaction()
                     transaction.replace(R.id.frameLayout, NewFragment()).commit()
                     return true
                 }
                 R.id.menu_note -> {
+                    val transaction = manager.beginTransaction()
                     transaction.replace(R.id.frameLayout, NoteFragment()).commit()
                     return true
                 }
             }
             return false
         }
-   }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.setting, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.setting_btn) {
+            //切換頁面
+            return super.onOptionsItemSelected(item)
+        }
+        return false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-         //預設葉面
+        getSupportActionBar()?.hide() //隱藏標題
+        // 隱藏狀態
+        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN)
+        val transaction = manager.beginTransaction()
+        //預設葉面
         transaction.replace(R.id.frameLayout, DayFragment()).commit()
 
         //menu listener
-        var menubtn=findViewById<BottomNavigationView>(R.id.menu_btn)
+        val menubtn=findViewById<BottomNavigationView>(R.id.menu_btn)
         menubtn.setOnNavigationItemSelectedListener(listener)
-
+        menubtn.setSelectedItemId(R.id.menu_day)
 
 
         val calender = Calendar.getInstance(TimeZone.getDefault(),Locale.TAIWAN)
