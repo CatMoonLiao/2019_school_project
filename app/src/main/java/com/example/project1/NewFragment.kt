66dda9b20ed807a7b14659c32b18701b2b79b6df
CatMoonLiao <nicole951527@gmail.com>
2020-04-01 20:00:35 +0800

    彩色下拉完成

--- app/src/main/java/com/example/project1/NewFragment.kt
+++ app/src/main/java/com/example/project1/NewFragment.kt
@@ -14,40 +14,72 @@ import android.widget.BaseAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_new.*
+import java.text.SimpleDateFormat
 import java.util.*
-//TODO:預設填入現在時間、關掉鍵盤
+
 class NewFragment : Fragment() {
-    private var isallday=false
+    private var isallday = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_new, container, false)
     }
 
     override fun onStart() {
         super.onStart()
+        initial()
+
+        //set color list
         val res: Resources = resources
         val colorName = res.getStringArray(R.array.color_name)
-        val backgroundColor=res.getIntArray(R.array.item_color)
-        color_spinner.adapter= context?.let { ColorAdapter(it,colorName,backgroundColor) }
-        //隱藏輸入鍵盤????
-        starttime.hideKeyboard()
-        endtime.hideKeyboard()
+        val backgroundColor = res.getIntArray(R.array.item_color)
+        color_spinner.adapter = context?.let { ColorAdapter(it, colorName, backgroundColor) }
+
         allday_switch.setOnCheckedChangeListener { _, isChecked ->
             isallday = isChecked
         }
         starttime.setOnClickListener(listener1)
         endtime.setOnClickListener(listener2)
+
+        ok_btn.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) =
+            //TODO("Save Data") //To change body of created functions use File | Settings | File Templates.
+                initial()
+        })
+        cancel_btn.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+                initial()
+            }
+        })
+
+    }
+
+    private fun initial() {
+        //button時間設為現在時間
+        lateinit var formatter: SimpleDateFormat
+        if (isallday)
+            formatter = SimpleDateFormat("yyyy/MM/dd")
+        else
+            formatter = SimpleDateFormat("yyyy/MM/dd HH:mm")
+
+        var curDate = Date(System.currentTimeMillis()) // 獲取當前時間
+        var str = formatter.format(curDate)
+        starttime.setText(str)
+        curDate.hours += 1
+        str = formatter.format(curDate)
+        endtime.setText(str)
+
+        //清空標題
+        title_text.setText("")
     }
 
 
-    private var listener1=object :View.OnClickListener{
+    private var listener1 = object : View.OnClickListener {
         override fun onClick(v: View?) {
-            var input:String=""
+            var input: String = ""
             val c = Calendar.getInstance()
             val year = c.get(Calendar.YEAR)
             val month = c.get(Calendar.MONTH)
@@ -65,21 +97,21 @@ class NewFragment : Fragment() {
                         Log.e("day", input)
 
                         //call time picker
-                        if(!isallday){
+                        if (!isallday) {
                             val hour = c.get(Calendar.HOUR)
                             val minute = c.get(Calendar.MINUTE)
-                            val timePickerDialog=TimePickerDialog(
+                            val timePickerDialog = TimePickerDialog(
                                 it,
                                 TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-                                    var h="$hourOfDay"
-                                    var min="$minute"
-                                    if(hourOfDay<10){
-                                        h="0$hourOfDay"
+                                    var h = "$hourOfDay"
+                                    var min = "$minute"
+                                    if (hourOfDay < 10) {
+                                        h = "0$hourOfDay"
                                     }
-                                    if(minute<10){
-                                        min="0$minute"
+                                    if (minute < 10) {
+                                        min = "0$minute"
                                     }
-                                    input+=" $h:$min"
+                                    input += " $h:$min"
                                     starttime.setText(input)
                                 },
                                 hour,
@@ -98,9 +130,9 @@ class NewFragment : Fragment() {
         }
     }
 
-    private var listener2=object :View.OnClickListener{
+    private var listener2 = object : View.OnClickListener {
         override fun onClick(v: View?) {
-            var input:String=""
+            var input: String = ""
             val c = Calendar.getInstance()
             val year = c.get(Calendar.YEAR)
             val month = c.get(Calendar.MONTH)
@@ -115,21 +147,21 @@ class NewFragment : Fragment() {
                         endtime.setText(input)
 
                         //call time picker
-                        if(!isallday){
+                        if (!isallday) {
                             val hour = c.get(Calendar.HOUR)
                             val minute = c.get(Calendar.MINUTE)
-                            val timePickerDialog=TimePickerDialog(
+                            val timePickerDialog = TimePickerDialog(
                                 it,
                                 TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-                                    var h="$hourOfDay"
-                                    var min="$minute"
-                                    if(hourOfDay<10){
-                                        h="0$hourOfDay"
+                                    var h = "$hourOfDay"
+                                    var min = "$minute"
+                                    if (hourOfDay < 10) {
+                                        h = "0$hourOfDay"
                                     }
-                                    if(minute<10){
-                                        min="0$minute"
+                                    if (minute < 10) {
+                                        min = "0$minute"
                                     }
-                                    input+=" $h:$min"
+                                    input += " $h:$min"
                                     endtime.setText(input)
                                 },
                                 hour,
@@ -146,10 +178,5 @@ class NewFragment : Fragment() {
             }
             dpd?.show()
         }
-    }
-
-    fun View.hideKeyboard() {
-        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 }
